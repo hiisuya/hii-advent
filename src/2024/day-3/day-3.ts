b04f6b2626d904e70a3c530525d7f4b56be71427
hiisuya <hiisuya@gmail.com>
2024-12-14 00:02:34 -0600

    cleaned up a bit more

--- src/2024/day-3/day-3.ts
+++ src/2024/day-3/day-3.ts
@@ -8,7 +8,6 @@ function hasInvalidChars(str: string) : boolean{
 
 export function day3() : void {
     try {
-
         const mulStr: string = "mul(";
         const data: string = fs.readFileSync(filePath, 'utf8');
         let total: number = 0;
@@ -19,19 +18,6 @@ export function day3() : void {
         }
 
         for (let i = 0; i < indices.length; i++) {
-            // at the VERY most, an entire mul formula can be
-            // 8 characters long, ending at the closing )
-            // to determine if this is a valid formula
-            // we need to scan those 8 characters
-            // if there is a ) then we take the first one
-            // and split into a smaller string there
-            // then determine if there is anything in there
-            // that is not a number or a comma. if so, the mul is incorrent
-            // then we need to check the distance from the ) to the ,
-            // that will be the our second number
-            // we then check from the , to the ( and that will be our first
-            // number. That should give us everything we need
-
             let index = indices[i] + mulStr.length;
             let evaluation = data.slice(index, index + 8);
             if (!evaluation.includes(")")) {
@@ -52,7 +38,7 @@ export function day3() : void {
 
         console.log("Total:", total);
     } catch (err) {
-        console.error('Error reading file:', err);
+        console.error('Something broke:', err);
     }
 
     day3_part2();
